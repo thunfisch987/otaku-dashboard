6490957c87e3995ce7ce3563d7cdf75da9e65ae0
Oliver Sader <thunfisch987@gmail.com>
2025-01-04 23:41:19 +0100

    :card_file_box: (schema) add product schema

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -7,3 +7,16 @@ export const users = sqliteTable('users', {
 	avatar: text('avatar').notNull(),
 	createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
 });
+
+export const products = sqliteTable('products', {
+	id: integer('id').primaryKey({ autoIncrement: true }),
+	name: text('name').unique().notNull(),
+	picture: text('picture'),
+	price: integer('price').notNull(),
+	supplier: text('supplier').notNull(),
+	createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+	updatedAt: integer('updated_at', { mode: 'timestamp' }).notNull(),
+},
+(table) => [
+	check('supplier_check', sql`${table.supplier} IN ("HDJ", "Otaku")`),
+]);
