f338c5e9358eb369b3af731db1fba5e192d3434b
Oliver Sader <thunfisch987@gmail.com>
2025-01-04 23:44:52 +0100

    :card_file_box: (schema) edit user schema, add givenName, familyName and lastLogin

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,11 +1,15 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
+import { sqliteTable, text, integer, check } from 'drizzle-orm/sqlite-core';
 
 export const users = sqliteTable('users', {
 	id: integer('id').primaryKey({ autoIncrement: true }),
 	name: text('name').notNull(),
+	givenName: text('given_name').notNull(),
+	familyName: text('family_name').notNull(),
 	email: text('email').notNull().unique(),
 	avatar: text('avatar').notNull(),
 	createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+	lastLogin: integer('last_login', { mode: 'timestamp' }).notNull(),
 });
 
 export const products = sqliteTable('products', {
