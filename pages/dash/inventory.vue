cf55c61b5045bba477724ef8d383f11a1061c8f1
Oliver Sader <thunfisch987@gmail.com>
2025-01-06 00:06:38 +0100

    :sparkles: add Inventory page

--- pages/dash/inventory.vue
+++ pages/dash/inventory.vue
@@ -1,3 +1,48 @@
 <template>
-	<div>inventor</div>
+	<div class="flex  flex-wrap">
+		<Card class="flex-1">
+			<CardHeader>
+				<CardTitle>Create Product</CardTitle>
+			</CardHeader>
+			<CardContent>
+				<AutoForm
+					:schema="insertProductSchema"
+					@submit="(data) => submit(data)"
+				>
+					<Button type="submit">
+						Create Product
+					</Button>
+				</AutoForm>
+			</CardContent>
+		</Card>
+		<pre class="flex-1">{{ allProducts }}</pre>
+		<div
+			v-if="parsedAllProducts.data"
+			class="container py-10 mx-auto"
+		>
+			<ProductsDataTable
+				:columns="columns"
+				:data="parsedAllProducts.data"
+			/>
+		</div>
+		<pre>{{ parsedAllProducts.data }}</pre>
+	</div>
 </template>
+
+<script setup lang="ts">
+import type { z } from 'zod';
+import { insertProductSchema, productArraySchema, columns } from '~/components/products/columns';
+
+const { data: allProducts, refresh: refreshProducts } = await useFetch('/api/products');
+
+const parsedAllProducts = productArraySchema.safeParse(allProducts.value);
+
+async function submit(formData: z.infer<typeof insertProductSchema>) {
+	const { data: newProduct } = await useFetch('/api/products', {
+		method: 'post',
+		body: formData,
+	});
+	refreshProducts();
+	return { newProduct };
+}
+</script>
