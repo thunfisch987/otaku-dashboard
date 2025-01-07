b0fd5dfd04afbd1dc496b62e0d41cb4615dd3d65
Oliver Sader <thunfisch987@gmail.com>
2025-01-07 03:46:57 +0100

    :lipstick::sparkles: use Dialog instead of Card for creating Product with :sparkles:Toast:sparkles:

--- pages/dash/inventory.vue
+++ pages/dash/inventory.vue
@@ -1,10 +1,16 @@
 <template>
-	<div class="flex  flex-wrap">
-		<Card class="flex-1">
-			<CardHeader>
-				<CardTitle>Create Product</CardTitle>
-			</CardHeader>
-			<CardContent>
+	<div class="flex flex-wrap">
+		<Toaster />
+		<Dialog v-model:open="createProductDialogOpen">
+			<DialogTrigger as-child>
+				<Button>
+					Create new Product
+				</Button>
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Create Product</DialogTitle>
+				</DialogHeader>
 				<AutoForm
 					:schema="insertProductSchema"
 					@submit="(data) => submit(data)"
@@ -13,11 +19,11 @@
 						Create Product
 					</Button>
 				</AutoForm>
-			</CardContent>
-		</Card>
+			</DialogContent>
+		</Dialog>
 		<div
 			v-if="parsedAllProducts.data"
-			class="flex-1 container py-10 mx-auto"
+			class="flex-auto container py-10 mx-auto"
 		>
 			<ProductsDataTable
 				:columns="columns"
@@ -29,18 +35,35 @@
 
 <script setup lang="ts">
 import type { z } from 'zod';
+
 import { insertProductSchema, productArraySchema, columns } from '~/components/products/columns';
+import { useToast } from '~/components/ui/toast/use-toast';
 
-const { data: allProducts, refresh: refreshProducts } = await useFetch('/api/products');
+const { data: allProducts, refresh: refreshProducts } = await useFetch('/api/products', {
+	key: 'productFetching',
+	lazy: true,
+});
 
-const parsedAllProducts = productArraySchema.safeParse(allProducts.value);
+const createProductDialogOpen = useState<boolean>('createProductDialogState', () => false);
+
+const { toast } = useToast();
+
+const parsedAllProducts = computed(() => productArraySchema.safeParse(allProducts.value));
 
 async function submit(formData: z.infer<typeof insertProductSchema>) {
-	const { data: newProduct } = await useFetch('/api/products', {
+	createProductDialogOpen.value = false;
+	const { error, status } = await useFetch('/api/products', {
 		method: 'post',
 		body: formData,
 	});
+	if (status.value === 'error' && error.value && error.value.statusCode === 409) {
+		toast({
+			title: `Ein Produkt mit dem Namen "${formData.name}" existiert bereits schon!`,
+			description: 'Bitte wähle einen anderen Namen.',
+			variant: 'destructive',
+		});
+		return;
+	}
 	refreshProducts();
-	return { newProduct };
 }
 </script>
