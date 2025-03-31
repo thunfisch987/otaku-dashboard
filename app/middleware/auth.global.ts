e3520359c41cfe5a0fb8b0812c83d4f417c4ed29
Oliver Sader <thunfisch987@gmail.com>
2025-03-31 11:44:36 +0200

    dev forms

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
 	const { loggedIn } = useUserSession();
-	if (to.path === '/dash/forms') {
+	if (import.meta.dev && to.path === '/dash/forms') {
 		return;
 	}
 	if (!loggedIn.value && to.path !== '/') {
