bbd1a30ad59cffe90054cca8e77960d473343f3d
Oliver Sader <thunfisch987@gmail.com>
2025-06-07 12:43:31 +0200

    live update console logs

--- server/routes/ws/liveproducts.ts
+++ server/routes/ws/liveproducts.ts
@@ -7,19 +7,25 @@ export default defineWebSocketHandler({
 		peer.subscribe('liveproducts');
 		peer.publish('liveproducts', 'listenup');
 		peer.send('listenup');
+		console.log('open');
+		console.log(peer);
 	},
 	close(peer) {
 		clients.delete(peer);
 		peer.unsubscribe('liveproducts');
+		console.log(peer);
 	},
 });
 
 export function updateLatestBackendData() {
+	console.log('updateLatestBackendData');
 	// latestBackendData = data;
 	// Notify all connected clients about the new data
 	const peer = clients.entries().next();
 	if (!peer.done) {
 		const firstPeer = peer.value[0];
+		console.log('firstPeer');
+		console.log(firstPeer);
 		firstPeer.publish('liveproducts', 'plzrefetch');
 		firstPeer.send('plzrefetch');
 	}
