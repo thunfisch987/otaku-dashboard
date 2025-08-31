0868f5de6fcf6bc9cae4726e2047eaf8e68b3b6e
Oliver Sader <thunfisch987@gmail.com>
2025-08-31 18:33:40 +0200

    kbd shortcuts for tabs

--- app/app.vue
+++ app/app.vue
@@ -10,3 +10,23 @@
 		</UApp>
 	</div>
 </template>
+
+<script setup lang="ts">
+defineShortcuts({
+	meta_1: () => {
+		navigateTo('/dash/dashboard');
+	},
+	meta_2: () => {
+		navigateTo('/dash/forms');
+	},
+	meta_3: () => {
+		navigateTo('/dash/inventory');
+	},
+	meta_4: () => {
+		navigateTo('/dash/pointofsale');
+	},
+	meta_5: () => {
+		navigateTo('/dash/transactions');
+	},
+});
+</script>
