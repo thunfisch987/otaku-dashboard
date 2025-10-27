e286c7a105b649c7f1d55e40f0246ecf9be951d9
Oliver Sader <thunfisch987@gmail.com>
2025-10-27 20:18:13 +0100

    pwa usecredentials

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,50 +1,53 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-				modules: [
-				 '@nuxt/eslint',
-				 '@nuxt/image',
-				 'nuxt-auth-utils',
-				 'nuxt-typed-router',
-				 '@nuxt/ui',
-				 'convex-nuxt',
-				 '@compodium/nuxt',
-				 '@vite-pwa/nuxt',
-				],
-				css: ['~/assets/css/main.css'],
-				convex: {
-								url: process.env.CONVEX_URL,
-				},
-				devtools: { enabled: true },
-				app: {
-								head: {
-												charset: 'utf-8',
-												viewport: 'width=device-width, initial-scale=1',
-												title: 'Otaku-Dashboard',
-												htmlAttrs: {
-																lang: 'en',
-												},
-												meta: [],
-								},
-				},
-				devServer: { port: 3001 },
-				future: {
-								compatibilityVersion: 4,
-				},
-				experimental: {
-								writeEarlyHints: true,
-								asyncContext: true,
-								viteEnvironmentApi: true,
-								extractAsyncDataHandlers: true,
-								typescriptPlugin: true,
-				},
-				compatibilityDate: '2025-06-03',
-				nitro: {
-								experimental: {
-												tasks: true,
-												openAPI: true,
-								},
-				},
-				ui: {
-								colorMode: true,
-				},
+	modules: [
+		'@nuxt/eslint',
+		'@nuxt/image',
+		'nuxt-auth-utils',
+		'nuxt-typed-router',
+		'@nuxt/ui',
+		'convex-nuxt',
+		'@compodium/nuxt',
+		'@vite-pwa/nuxt',
+	],
+	css: ['~/assets/css/main.css'],
+	convex: {
+		url: process.env.CONVEX_URL,
+	},
+	devtools: { enabled: true },
+	app: {
+		head: {
+			charset: 'utf-8',
+			viewport: 'width=device-width, initial-scale=1',
+			title: 'Otaku-Dashboard',
+			htmlAttrs: {
+				lang: 'en',
+			},
+			meta: [],
+		},
+	},
+	devServer: { port: 3001 },
+	future: {
+		compatibilityVersion: 4,
+	},
+	experimental: {
+		writeEarlyHints: true,
+		asyncContext: true,
+		viteEnvironmentApi: true,
+		extractAsyncDataHandlers: true,
+		typescriptPlugin: true,
+	},
+	compatibilityDate: '2025-06-03',
+	nitro: {
+		experimental: {
+			tasks: true,
+			openAPI: true,
+		},
+	},
+	ui: {
+		colorMode: true,
+	},
+	pwa: {
+		useCredentials: true,
+	},
 });
