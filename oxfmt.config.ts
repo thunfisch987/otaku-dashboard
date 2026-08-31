8bc4effc4c1c4958d936810f6de3ecff555283d9
Oliver Sader <oliversader@proton.me>
2026-08-31 15:25:14 +0200

    fix oxfmt formatting in zed

---
+++ oxfmt.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'oxfmt';
+
+export default defineConfig({
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	printWidth: 80,
+	singleAttributePerLine: true,
+	sortPackageJson: false,
+});
